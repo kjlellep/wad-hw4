c4b47912578b103cd724d6332655da7ec443f4c6
Taavet <taavet.simo@gmail.com>
2019-12-14 21:26:01 +0200

    Created Test to test when an item in the list is marked as done  item is updated correctly

--- tests/unit/textTest.spec.js
+++ tests/unit/textTest.spec.js
@@ -7,5 +7,23 @@ describe('List text', () => {
     it('displays text "Add your first Todo task" with empty list', () => {
         const heading = wrapper.find('h3');
         expect(heading.text()).toBe("Add your first Todo task")
+    });
+
+    const testList = [
+        {id: 1,
+            title: 'Test 1',
+            done: false
+        }];
+
+    const wrapper2 = mount(List, {
+        propsData: {
+            list: testList
+        }
+    });
+
+    it('List item is updated correctly', () => {
+        const button = wrapper2.find('span');
+        button.trigger('click');
+        expect (wrapper2.props("list")[0].done).toBe(true);
     })
 });
